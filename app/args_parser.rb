32c9fc15adb28a2c0ddf838fee3b59400ad5b08e
Andres Pacheco <andrespach4@gmail.com>
2020-07-22 13:17:02 -0500

    Separating by methods

--- app/args_parser.rb
+++ app/args_parser.rb
@@ -5,39 +5,75 @@ directory_flag = ARGV[3]
 path = ARGV[4]
 
 class ArgsParser 
+  attr_accessor :register_flag, :port_flag, :port, :directory_flag, :path
+
   def initialize(register_flag, port_flag, port, directory_flag, path)
     @register_flag = register_flag
     @port_flag = port_flag
     @port = port
     @directory_flag = directory_flag
     @path = path
+    @error_message = 'ok'
   end
 
-  def valid_ok
-    if @register_flag == '-l' && @port_flag == '-p' && @port == 8080 && @directory_flag == '-d' && @path == '/usr/ading'
-      'Todo ok!'
+  def arguments_validation
+    register_flag_exist? && port_flag_exist? && port_exist? && directory_flag_exist? && path_exist?
+  end
+
+  def register_flag_exist?
+    if register_flag == '-l'
+      true
     else
-      'Algo a salido mal, asegurese de mandar valores correctos'
-      end
-  end
-
-  def port
-    if @port == nil
-      'Devera ingreasar un valor en port'
+      @error_message = 'Falta agregar valor a register_flag'
+      false
     end
   end
 
-  def path
-    if @path == nil
-      'Devera ingresar un path'
+  def port_flag_exist?
+    if port_flag == '-p'
+      true
+    else
+      @error_message = 'Falta agregar valor a port_flag'
+      false
     end
+  end
+
+  def port_exist?
+    if port == '8080'
+      true
+    else
+      @error_message = 'Falta agregar valor a port'
+      false
+    end 
+  end
+
+  def directory_flag_exist?
+    if directory_flag == '-d'
+      true
+    else
+      @error_message = 'Falta agregar valor a directory_flag'
+      false
+    end
+  end
+
+  def path_exist?
+    if path != nil
+      true
+    else
+      @error_message = 'Falta agregar valor a path'
+      false
+    end
+  end
+
+  def message_error
+    @error_message
   end
 end
 
 
-a = Prueba.new('-l', '-p', 8080, '-d', '/usr/ading')
-puts a.valid_ok
-
+a = ArgsParser.new(register_flag, port_flag, port, directory_flag, path)
+a.arguments_validation
+puts a.message_error
 
 
 
